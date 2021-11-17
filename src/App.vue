b05932305599ba3607df4f491f11c3e8b2fa1cb5
Torben Carstens <git@carstens.tech>
2021-11-18 00:53:35 +0100

    Update link color

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,9 @@ body {
   background-color: #225522;
   color: #9e9a93;
 }
+a {
+  color: #bebaf3;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
