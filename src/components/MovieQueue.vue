b731ae5ea927e4ba3865181022b29e4dc656ced2
Torben Carstens <git@carstens.tech>
2022-05-06 17:54:22 +0200

    Formatting

--- src/components/MovieQueue.vue
+++ src/components/MovieQueue.vue
@@ -8,11 +8,13 @@
     <li class="queue-item" v-for="(movie, index) in data" :key="index">
       <div>
         <h2 class="queue-item-header">
-          <a :href="'https://imdb.com/title/tt' + movie.imdb.id" class="queue-item-imdb-title" target="_blank">{{ movie.imdb.title }}</a>
+          <a :href="'https://imdb.com/title/tt' + movie.imdb.id" class="queue-item-imdb-title"
+             target="_blank">{{ movie.imdb.title }}</a>
           ({{ movie.imdb.year }}) {{ movie.imdb.rating }}⭐
         </h2>
 
-        <img :src="movie.imdb.cover.url" v-bind:alt="'cover for ' + movie.imdb.title" class="queue-item-cover-image" :width=250 :height="250 * movie.imdb.cover.ratio" />
+        <img :src="movie.imdb.cover.url" v-bind:alt="'cover for ' + movie.imdb.title" class="queue-item-cover-image"
+             :width=250 :height="250 * movie.imdb.cover.ratio"/>
       </div>
     </li>
   </ul>
@@ -24,8 +26,8 @@
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
-import { defineComponent } from "@vue/composition-api";
+import {onMounted, ref} from "vue";
+import {defineComponent} from "@vue/composition-api";
 
 const data = ref([]);
 const loading = ref(true);
@@ -47,7 +49,7 @@ function fetchMovie(id) {
 
     return res.json();
   })
-  .catch(err => err)
+      .catch(err => err)
 }
 
 function getCookie(name) {
@@ -57,52 +59,53 @@ function getCookie(name) {
 }
 
 function fetchData(initial) {
-    loading.value = initial;
-    queueCount.value = initial;
+  loading.value = initial;
+  queueCount.value = initial;
 
-    return fetch('https://api.timhatdiehandandermaus.consulting/queue', {
-        method: 'get',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(res => {
-        if (!res.ok) {
-            const error = new Error(res.statusText);
-            error.json = res.json();
-            throw error;
-        }
+  return fetch('https://api.timhatdiehandandermaus.consulting/queue', {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => {
+    if (!res.ok) {
+      const error = new Error(res.statusText);
+      error.json = res.json();
+      throw error;
+    }
 
-        return res.json();
-    }).then(json => Promise.all(json.queue.map(async (queueItemResponse, index) => {
-          data.value = [];
-          if (getCookie("enableBoringQueueCounter") === "true") {
-            queueCount.value = json.queue.length;
-          }
+    return res.json();
+  }).then(json => Promise.all(json.queue.map(async (queueItemResponse, index) => {
+    data.value = [];
+    if (getCookie("enableBoringQueueCounter") === "true") {
+      queueCount.value = json.queue.length;
+    }
 
-          fetchMovie(queueItemResponse.id)
-            .then(m => {
-              m.index = index;
-              data.value.push(m);
-              loading.value = false;
-            })
-            .catch(err => {
-              console.error("[ fetchMovie: ", err, " ]");
-              return err;
-            })})))
-        .then(() => {
-          new Promise(resolve => setTimeout(resolve, 800))
-              .then(() => {
-                data.value.sort((m1, m2) => m1.index - m2.index);
-              })
+    fetchMovie(queueItemResponse.id)
+        .then(m => {
+          m.index = index;
+          data.value.push(m);
+          loading.value = false;
         })
-    .catch(err => {
+        .catch(err => {
+          console.error("[ fetchMovie: ", err, " ]");
+          return err;
+        })
+  })))
+      .then(() => {
+        new Promise(resolve => setTimeout(resolve, 800))
+            .then(() => {
+              data.value.sort((m1, m2) => m1.index - m2.index);
+            })
+      })
+      .catch(err => {
         error.value = err;
         if (err.json) {
-            return err.json.then(json => {
-                error.value.message = json.message;
-            });
+          return err.json.then(json => {
+            error.value.message = json.message;
+          });
         }
-    })
+      })
 }
 
 export default defineComponent({
